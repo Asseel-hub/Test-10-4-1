3031b91e96e0ef05be43cddbcbdff5cfb07b18e1
Asseel-hub <asseelyacoub@.com>
2025-04-10 15:54:53 +0300

    update for the login , ....

---
+++ src/demoTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class demoTest {
+	
+	@Test
+	public void PrintSel() {
+		
+		
+		System.out.print("this is a selenium demo 1");
+		
+		
+	}
+	
+	
+	
+	
+
+}
